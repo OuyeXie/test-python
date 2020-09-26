31eb7f11fc4bbeeb154231825ad15af453a32795
Ouye Xie <ouyexie@amazon.com>
2020-09-26 13:49:58 -0700

    fix ctype, predict part 1

--- gluon/gluon_crash.py
+++ gluon/gluon_crash.py
@@ -377,8 +377,7 @@ class GluonCrash:
 
         # create a validation dataset and data loader
         mnist_valid = gluon.data.vision.FashionMNIST(train=False)
-        # TODO: fix ValueError: ctypes objects containing pointers cannot be pickled
-        #  try Linux
+        # This works on Linux with Conda env test-python
         valid_data = gluon.data.DataLoader(
             mnist_valid.transform_first(transformer),
             batch_size=batch_size, num_workers=4)
@@ -427,9 +426,51 @@ class GluonCrash:
         # save the trained parameters onto disk
         net.save_parameters('net.params')
 
+    def testPredictNNWithSavedParams(self):
+        # copy a simple model’s definition
+        net = nn.Sequential()
+        net.add(nn.Conv2D(channels=6, kernel_size=5, activation='relu'),
+                nn.MaxPool2D(pool_size=2, strides=2),
+                nn.Conv2D(channels=16, kernel_size=3, activation='relu'),
+                nn.MaxPool2D(pool_size=2, strides=2),
+                nn.Flatten(),
+                nn.Dense(120, activation="relu"),
+                nn.Dense(84, activation="relu"),
+                nn.Dense(10))
+
+        # load params back
+        net.load_parameters('net.params')
+
+        transformer = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(0.13, 0.31)])
+
+        mnist_valid = datasets.FashionMNIST(train=False)
+        X, Y = mnist_valid[:10]
+        preds = []
+        for x, y in zip(X,Y):
+            x = transformer(x).expand_dims(axis=0)
+            pred = net(x).argmax(axis=1)
+            preds.append((pred.astype('int32').asscalar(), y))
+        print(preds)
+
+
+        # _, figs = plt.subplots(1, 10, figsize=(15, 15))
+        # text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+        #                'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+        # display.set_matplotlib_formats('svg')
+        # for f, x, yi, pyi in zip(figs, X, y, preds):
+        #     f.imshow(x.reshape((28, 28)).asnumpy())
+        #     ax = f.axes
+        #     ax.set_title(text_labels[yi] + '\n' + text_labels[pyi])
+        #     ax.title.set_fontsize(14)
+        #     ax.get_xaxis().set_visible(False)
+        #     ax.get_yaxis().set_visible(False)
+        # plt.show()
 
 if __name__ == "__main__":
     gluonCrash: GluonCrash = GluonCrash()
+
     # gluonCrash.testNdArray()
 
     # gluonCrash.testNNDense()
@@ -441,4 +482,5 @@ if __name__ == "__main__":
 
     # gluonCrash.testAutoGrad()
 
-    gluonCrash.testTrainNNWithFashionMNIST()
+    # gluonCrash.testTrainNNWithFashionMNIST()
+    gluonCrash.testPredictNNWithSavedParams()
